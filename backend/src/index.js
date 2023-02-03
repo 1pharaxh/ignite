a49e68cb7ec0c3eeb6baa8f8c3883a1e12956c99
Akarshan Mishra <pharaohsupreme03@gmail.com>
2023-02-02 22:32:15 -0700

    fixed merge
    fixed merge

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"); // MongoDB ODM
 
 const port = process.env.PORT || 3000;
 const dbUri = process.env.DB_URI;
