03c64a2aeebced5732c91a8a1e8381e3c1ad3cfa
Akarshan Mishra <93630550+1pharaxh@users.noreply.github.com>
2023-02-02 22:26:24 -0700

    Revert "Backend"

---
+++ frontend/src/pages/HowToApply.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HowToApply() {
+    return (
+        <div>
+            <h1>This is how to apply page.</h1>
+        </div>
+    )
+}
+
+export default HowToApply
