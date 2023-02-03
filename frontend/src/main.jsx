b669cbf6437a3548e5954550124440269e0ce18c
Akarshan Mishra <pharaohsupreme03@gmail.com>
2023-02-02 23:36:03 -0700

    added analytics

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './static/css/index.css'
 import App from './App'
+import { inject } from '@vercel/analytics';
+
+inject();
 
 ReactDOM.render(
   <React.StrictMode>
