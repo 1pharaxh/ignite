777d014d2770a0f0a64039a0084d65178c6c09e0
Akarshan Mishra <pharaohsupreme03@gmail.com>
2023-02-03 23:45:22 -0700

    added loading
    added loading

--- frontend/src/pages/Companies.jsx
+++ frontend/src/pages/Companies.jsx
@@ -1,21 +1,35 @@
 import React, { useState, useEffect } from 'react'
+import { HashLoader } from 'react-spinners';
 
 function Companies() {
     const googleDriveImage = 'https://drive.google.com/uc?export=view&id='
-    const [companies, setCompanies] = useState([])
+    const [companies, setCompanies] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const override = {
+        display: "block",
+        margin: "0 auto",
+        padding: "0",
+        opacity: 1,
+
+        // borderColor: "teal",
+    };
 
     useEffect(() => {
         async function fetchData() {
             const response = await fetch('https://ignite-25g9.onrender.com/companies')
             const data = await response.json()
             setCompanies(data)
+            setLoading(false)
         }
         fetchData()
     }, [])
     return (
         <div className='flex flex-col relative h-full w-full'>
+
+
             {/*TEAL COLOR*/}
-            <div className='overflow-visible flex flex-col  mx-0 bg-teal-600 h-80 w-full overflow-auto'>
+            <div className={`${!loading ? `opacity-100` : `opacity-50`} overflow-visible flex flex-col  mx-0 bg-teal-600 h-80 w-full overflow-auto`}>
                 <div className='flex items-center justify-center w-full h-full'>
                     <h1 className='text-5xl text-white font-medium content-center'> Find your favourite company!</h1>
                 </div>
@@ -28,9 +42,11 @@ function Companies() {
                         </button>
                     </div>
                 </div>
+
             </div>
-            <h1 className='text-4xl text-teal-700 font-medium content-center my-16 mx-16'> Our top recruiters!</h1>
-            <div className="grid grid-cols-4 gap-4 px-16">
+            <HashLoader cssOverride={override} size={150} color={"#0f766e"} loading={loading} />
+            <h1 className={`${!loading ? `opacity-100` : `opacity-50`} text-4xl text-teal-700 font-medium content-center my-16 mx-16`}> Our top recruiters!</h1>
+            <div className={`${!loading ? `opacity-100` : `opacity-50`} grid grid-cols-4 gap-4 px-16`}>
 
                 {companies.map(company => (
                     < div key={company._id} className='bg-teal-700 h-44 w-9/12 items-center  rounded-xl p-5 justify-center flex flex-col' >
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+
+
 
 function Home() {
+
   return (
-    <div>
-      <h1>This is home page.</h1>
+    <div className="">
+      <h1>Home Page</h1>
     </div>
   )
 }
