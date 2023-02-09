c3997c6da6de495bb5ad5d6fe4e8627074a8514d
Akarshan Mishra <pharaohsupreme03@gmail.com>
2023-02-09 16:46:08 -0700

    added search

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
-const mongoose = require("mongoose"); // MongoDB ODM
+const mongoose = require("mongoose");
 
 const port = process.env.PORT || 3000;
 const dbUri = process.env.DB_URI;
@@ -25,11 +25,18 @@ mongoose
 const companySchema = new mongoose.Schema({
   name: String,
   image: String,
+  about: {
+    download_job_descrip: String,
+    about_comp: String,
+    website: String,
+    work_location: String,
+    job_profile_description: Array,
+    profile: Object,
+  },
 });
-
 const Company = mongoose.model("companies", companySchema);
 
-app.get("/getAll", (req, res) => {
+app.get("/companies", (req, res) => {
   Company.find({}, (err, companies) => {
     if (err) {
       return res.status(500).send(err);
@@ -39,6 +46,38 @@ app.get("/getAll", (req, res) => {
   });
 });
 
+app.get("/companies/:id", (req, res) => {
+  const id = req.params.id;
+  Company.findOne({ _id: id }, (err, company) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    console.log(company);
+    return res.status(200).send(company);
+  });
+});
+
+app.get("/search/:key", (req, res) => {
+  const key = req.params.key;
+  Company.find(
+    {
+      $or: [
+        { name: { $regex: key, $options: "i" } },
+        { "about.download_job_descrip": { $regex: key, $options: "i" } },
+        { "about.about_comp": { $regex: key, $options: "i" } },
+        { "about.website": { $regex: key, $options: "i" } },
+        { "about.work_location": { $regex: key, $options: "i" } },
+      ],
+    },
+    (err, companies) => {
+      if (err) {
+        return res.status(500).send(err);
+      }
+      console.log(companies);
+      return res.status(200).send(companies);
+    }
+  );
+});
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
