ce08241addbf246b69a3a9772cbdf7e5afc18852
Akarshan Mishra <pharaohsupreme03@gmail.com>
2023-03-23 17:08:42 -0600

    changed DB schema

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json()); // parse incoming request
 app.use(express.urlencoded({ extended: true })); // Parse incoming request bodies with URL-encoded payloads
 const corsOptions = {
-//   origin: ["http://localhost:3000", "https://admin-ignite.vercel.app/", "https://anubhava.ignitesgtb.com"],
+  //   origin: ["http://localhost:3000", "https://admin-ignite.vercel.app/", "https://anubhava.ignitesgtb.com"],
   origin: "*",
   optionsSuccessStatus: 200,
 };
@@ -37,16 +37,17 @@ mongoose
 const companySchema = new mongoose.Schema({
   name: String,
   image: String,
-  about: {
-    download_job_descrip: String,
-    about_comp: String,
-    website: String,
-    paid_unpaid: String,
-    work_location: String,
-    job_profile_description: Array,
-    profile: Object,
-  },
+  pdfDescription: String,
+  about_comp: String,
+  website: String,
+  work_location: String,
+  paid_unpaid: String,
+  job_profile_description: Object,
+  perks: Array,
+  eligibility: Array,
 });
+
+// return all companies
 const Company = mongoose.model("companies", companySchema);
 app.get("/companies", (req, res) => {
   Company.find({}, (err, companies) => {
@@ -58,25 +59,7 @@ app.get("/companies", (req, res) => {
   });
 });
 
-app.get("/filter/:key", (req, res) => {
-  const key = req.params.key;
-  Company.find(
-    {
-      $or: [
-        { "about.paid_unpaid": { $regex: '\\b' + key + '\\b', $options: "i" } },
-        { "about.work_location": { $regex: key, $options: "i" } },
-      ],
-    },
-    (err, companies) => {
-      if (err) {
-        return res.status(500).send(err);
-      }
-      console.log(companies);
-      return res.status(200).send(companies);
-    }
-  );
-});
-
+// return company by id
 app.get("/companies/:id", (req, res) => {
   const id = req.params.id;
   Company.findOne({ _id: id }, (err, company) => {
@@ -88,6 +71,7 @@ app.get("/companies/:id", (req, res) => {
   });
 });
 
+// Search companies
 app.get("/search/:key", (req, res) => {
   const key = req.params.key;
   Company.find(
@@ -95,14 +79,14 @@ app.get("/search/:key", (req, res) => {
       $or: [
         { name: { $regex: key, $options: "i" } },
         {
-          "about.job_profile_description": {
+          job_profile_description: {
             $elemMatch: { $elemMatch: { $regex: key, $options: "i" } },
           },
         },
-        { "about.paid_unpaid": { $regex: '\\b' + key + '\\b', $options: "i" } },
-        { "about.about_comp": { $regex: key, $options: "i" } },
-        { "about.website": { $regex: key, $options: "i" } },
-        { "about.work_location": { $regex: key, $options: "i" } },
+        { paid_unpaid: { $regex: "\\b" + key + "\\b", $options: "i" } },
+        { about_comp: { $regex: key, $options: "i" } },
+        { website: { $regex: key, $options: "i" } },
+        { work_location: { $regex: key, $options: "i" } },
       ],
     },
     (err, companies) => {
@@ -114,6 +98,8 @@ app.get("/search/:key", (req, res) => {
     }
   );
 });
+
+// Delete company by id
 app.delete("/delete" + postUri + "/companies/:id", (req, res) => {
   const id = req.params.id;
   Company.findOneAndDelete({ _id: id }, (err, company) => {
@@ -125,15 +111,33 @@ app.delete("/delete" + postUri + "/companies/:id", (req, res) => {
   });
 });
 
+// Create company
 app.post(postUri, (req, res) => {
-  if (!req.body.name || !req.body.image || !req.body.about) {
+  if (
+    !req.body.name ||
+    !req.body.image ||
+    !req.body.about_comp ||
+    !req.body.website ||
+    !req.body.work_location ||
+    !req.body.paid_unpaid ||
+    !req.body.job_profile_description ||
+    !req.body.perks ||
+    !req.body.eligibility
+  ) {
     return res.status(400).send("Missing required fields");
   }
 
   const company = new Company({
     name: req.body.name,
     image: req.body.image,
-    about: req.body.about,
+    pdfDescription: req.body.pdfDescription,
+    about_comp: req.body.about_comp,
+    website: req.body.website,
+    work_location: req.body.work_location,
+    paid_unpaid: req.body.paid_unpaid,
+    job_profile_description: req.body.job_profile_description,
+    perks: req.body.perks,
+    eligibility: req.body.eligibility,
   });
 
   company.save((err, savedCompany) => {
