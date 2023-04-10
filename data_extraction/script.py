d19b91d70a412963d9da313fcfff79751720718a
Akarshan Mishra <pharaohsupreme03@gmail.com>
2023-04-10 14:29:59 -0600

    Updated script and disabled apply button

--- data_extraction/script.py
+++ data_extraction/script.py
@@ -135,41 +135,110 @@ db = firestore.client()
 # Reference to the applications collection
 users_ref = db.collection('users')
 applications = users_ref.get()
-# print all the data in the applications collection
-for application in applications:
-    name = application.to_dict()['name']
-    resume = application.to_dict()['resume']
-    email = application.to_dict()['email']
-    phone = application.to_dict()['contactNumber']
-    college = application.to_dict()['college']
-    course = application.to_dict()['course']
-    year = application.to_dict()['yearOfStudy'] + " Year"
-    userId = application.id
-    keys = application.to_dict()['applied']
-    os.system('cls' if os.name == 'nt' else 'clear')
-    for key in keys:
-        pipeline = [
-            {"$match": {f"job_profile_description.{key}": {"$exists": True}}},
-            {"$project": {
-            "name": 1,
-            "job": {"$arrayElemAt": [{"$arrayElemAt": [f"$job_profile_description.{key}", 0]}, 0]}}}
-        ]
-        # Run the aggregation pipeline
-        results = collection.aggregate(pipeline)
+# Ask if the user wants to check applications or see registered users
+os.system('cls' if os.name == 'nt' else 'clear')
+choice  = input('''
+                                            :
+                                        :::
+                    '::                   ::::
+                    '::::.     .....:::.:::::::
+                    '::::::::::::::::::::::::::::
+                    ::::::XUWWWWWU:::::XW$$$$$$WX:
+                    ::::X$$$$$$$$$$W::X$$$$$$$$$$Wh
+                ::::t$$$$$$$$$$$$W:$$$$$$P*$$$$M::
+                :::X$$$$$$""""$$$$X$$$$$   ^$$$$X:::
+                ::::M$$$$$$    ^$$$RM$$$L    <$$$X::::
+                .:::::M$$$$$$     $$$R:$$$$.   d$$R:::`
+            '~::::::?$$$$$$...d$$$X$6R$$$$$$$$RXW$X:'`
+                '~:WNWUXT#$$$$$$$$TU$$$$W6IBBIW@$$RX:
+                `~
+1. Check Applications
+2. See Registered Users
 
-        # Iterate over the results and print the value of the name field
-        for result in results:
-            print([userId, name, resume, email, phone, college, result["name"], result["job"], course, year]);
-            with open('applications.csv', 'a', newline='') as file:
-                writer = csv.writer(file)
-                # set the header
-                if os.stat('applications.csv').st_size == 0:
-                    writer.writerow(['User ID', 'Name', 'Resume Link', 'Email', 'Phone', 'College', 'Company', 'Job', 'College Course', 'Degree Year'])
-                    # leave a blank line
-                    writer.writerow([])
-                writer.writerow([userId, name, resume, email, phone, college, result["name"], result["job"], course, year])
-            file.close()
-    time.sleep(1)
+Enter 1 or 2:
+''')
+# wait till the user enters a valid choice
+while choice != "1" and choice != "2":
+    os.system('cls' if os.name == 'nt' else 'clear')
+    choice  = input('''
+                                            :
+                                        :::
+                    '::                   ::::
+                    '::::.     .....:::.:::::::
+                    '::::::::::::::::::::::::::::
+                    ::::::XUWWWWWU:::::XW$$$$$$WX:
+                    ::::X$$$$$$$$$$W::X$$$$$$$$$$Wh
+                ::::t$$$$$$$$$$$$W:$$$$$$P*$$$$M::
+                :::X$$$$$$""""$$$$X$$$$$   ^$$$$X:::
+                ::::M$$$$$$    ^$$$RM$$$L    <$$$X::::
+                .:::::M$$$$$$     $$$R:$$$$.   d$$R:::`
+            '~::::::?$$$$$$...d$$$X$6R$$$$$$$$RXW$X:'`
+                '~:WNWUXT#$$$$$$$$TU$$$$W6IBBIW@$$RX:
+                `~
+            1. Check Applications
+            2. See Registered Users
+
+            Enter 1 or 2:
+            ''')
+    
+if choice == "1":
+    for application in applications:
+        name = application.to_dict()['name']
+        resume = application.to_dict()['resume']
+        email = application.to_dict()['email']
+        phone = application.to_dict()['contactNumber']
+        college = application.to_dict()['college']
+        course = application.to_dict()['course']
+        year = application.to_dict()['yearOfStudy'] + " Year"
+        userId = application.id
+        keys = application.to_dict()['applied']
+        os.system('cls' if os.name == 'nt' else 'clear')
+        for key in keys:
+            pipeline = [
+                {"$match": {f"job_profile_description.{key}": {"$exists": True}}},
+                {"$project": {
+                "name": 1,
+                "job": {"$arrayElemAt": [{"$arrayElemAt": [f"$job_profile_description.{key}", 0]}, 0]}}}
+            ]
+            # Run the aggregation pipeline
+            results = collection.aggregate(pipeline)
+            # check if we have any results
+            # Iterate over the results and print the value of the name field
+            for result in results:
+                print([userId, name, resume, email, phone, college, result["name"], result["job"], course, year]);
+                with open('applications.csv', 'a', newline='') as file:
+                    writer = csv.writer(file)
+                    # set the header
+                    if os.stat('applications.csv').st_size == 0:
+                        writer.writerow(['User ID', 'Name', 'Resume Link', 'Email', 'Phone', 'College', 'Company', 'Job', 'College Course', 'Degree Year'])
+                        # leave a blank line
+                        writer.writerow([])
+                    writer.writerow([userId, name, resume, email, phone, college, result["name"], result["job"], course, year])
+                file.close()
+        time.sleep(1)
+if choice == "2":
+    os.system('cls' if os.name == 'nt' else 'clear')
+    for application in applications:
+        name = application.to_dict()['name']
+        resume = application.to_dict()['resume']
+        email = application.to_dict()['email']
+        phone = application.to_dict()['contactNumber']
+        college = application.to_dict()['college']
+        course = application.to_dict()['course']
+        year = application.to_dict()['yearOfStudy'] + " Year"
+        userId = application.id
+        print([userId, name, resume, email, phone, college, course, year]);
+        with open('applications.csv', 'a', newline='') as file:
+            writer = csv.writer(file)
+            # set the header
+            if os.stat('applications.csv').st_size == 0:
+                writer.writerow(['User ID', 'Name', 'Resume Link', 'Email', 'Phone', 'College', 'College Course', 'Degree Year'])
+                # leave a blank line
+                writer.writerow([])
+            writer.writerow([userId, name, resume, email, phone, college, course, year])
+        file.close()
+        time.sleep(1)
+
 os.system('cls' if os.name == 'nt' else 'clear')
 print('''
 ⣻⡿⢏⢥⡻⢬⡍⢳⡘⢦⢣⢝⡢⡓⡜⠰⢂⠐⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢻⣦⠀
@@ -199,7 +268,7 @@ print('''
 ⣿⣻⡿⣯⣿⣻⣽⣿⣳⡿⣯⣿⣟⣿⢾⣻⡿⣽⣯⣷⣿⣻⣽⣯⣿⣽⣾⣳⣯⣿⣽⣾⣽⣻⣽⣞⡿⣽⡾⣽⢯⣟⣶⣻⡵⣏⡷⣹⠶⣽
 
 
- /$$$$$$$  
+/$$$$$$$  
 | $$__  $$    
 | $$  \ $$  /$$$$$$  /$$$$$$$   /$$$$$$    
 | $$  | $$ /$$__  $$| $$__  $$ /$$__  $$  
@@ -207,7 +276,7 @@ print('''
 | $$  | $$| $$  | $$| $$  | $$| $$_____/                                                    
 | $$$$$$$/|  $$$$$$/| $$  | $$|  $$$$$$$                                                    
 |_______/  \______/ |__/  |__/ \_______/     
-                                                                                               
+                                                                                            
     File generated as "applications.csv" in the same folder.                                                                                  
 ''')
 # wait for user input to exit
