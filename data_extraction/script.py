17aa0882b51f3d2a7406f07de6c8382c7d2826a4
Akarshan Mishra <pharaohsupreme03@gmail.com>
2023-04-20 10:20:17 -0600

    Changed Banner

--- data_extraction/script.py
+++ data_extraction/script.py
@@ -1,14 +1,34 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
+
 import csv
 import time
 from pymongo import MongoClient
 import json
+import subprocess
+import sys
 import os 
 import pyautogui
 pyautogui.hotkey('alt', 'enter')
 # clear the terminal
+try:
+    #check if node is installed
+    subprocess.check_call(['node', '--version'], stdout=subprocess.DEVNULL)
+    print("node is installed")
+
+except subprocess.CalledProcessError:
+    print("node is not installed please install node")
+    # open the node website
+    subprocess.run(["start", "https://nodejs.org/en/download/"], shell=True)
+    sys.exit(1)
+# run command to dump the database   
+cmd = 'npx -p node-firestore-import-export firestore-export -a serviceAccountKey.json -b backup.json'
+
+result = subprocess.run(['powershell', '-Command', cmd], shell=True, stdout=subprocess.PIPE)
+
 os.system('cls' if os.name == 'nt' else 'clear')
+
+
+
+
 
 print('''
 ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
@@ -75,14 +95,16 @@ if not os.path.exists('serviceAccountKey.json') or not os.path.exists('config.js
     input("Press Enter to exit...")
     exit()
 
-cred = credentials.Certificate("serviceAccountKey.json")
+# read the backup.json file
+with open('backup.json') as f:
+    data = json.load(f)
+data = data["__collections__"]["users"]
 config = None
 # Read the config file
 with open('config.json') as f:
     config = json.load(f)
 mongodb_url = config['mongodb_url']
 
-firebase_admin.initialize_app(cred)
 if os.path.exists('applications.csv'):
     # check if file is being used by another process
     try:
@@ -131,9 +153,7 @@ client = MongoClient(mongodb_url)
 db = client.Collection
 collection = db.companies
 
-db = firestore.client()
-# Reference to the applications collection
-users_ref = db.collection('users')
+
 
 # Ask if the user wants to check applications or see registered users
 os.system('cls' if os.name == 'nt' else 'clear')
@@ -155,11 +175,12 @@ choice  = input('''
 1. Check Applications
 2. See Registered Users
 3. See Incomplete User Profiles
+4. Number of Registered Users
 
-Enter 1 or 2 or 3:
+Enter 1 or 2 or 3 or 4:
 ''')
 # wait till the user enters a valid choice
-while choice != "1" and choice != "2" and choice != "3":
+while choice != "1" and choice != "2" and choice != "3" and choice != "4":
     os.system('cls' if os.name == 'nt' else 'clear')
     choice  = input('''
                                             :
@@ -176,26 +197,32 @@ while choice != "1" and choice != "2" and choice != "3":
             '~::::::?$$$$$$...d$$$X$6R$$$$$$$$RXW$X:'`
                 '~:WNWUXT#$$$$$$$$TU$$$$W6IBBIW@$$RX:
                 `~
-            1. Check Applications
-            2. See Registered Users
-            3. See Incomplete User Profiles
+1. Check Applications
+2. See Registered Users
+3. See Incomplete User Profiles
+4. Number of Registered Users
 
-            Enter 1 or 2 or 3:
-            ''')
+Enter 1 or 2 or 3 or 4:
+''')
     
 if choice == "1":
-    query = users_ref.where('applied', 'not-in', [[]])
-    applications = query.stream()
+    applications = []
+    for userId in data:
+        application = data[userId]
+        if "applied" in application and application['applied'] != []:
+            application["id"] = userId
+            applications.append(application)
+
     for application in applications:
-        name = application.to_dict()['name']
-        resume = application.to_dict()['resume']
-        email = application.to_dict()['email']
-        phone = application.to_dict()['contactNumber']
-        college = application.to_dict()['college']
-        course = application.to_dict()['course']
-        year = application.to_dict()['yearOfStudy'] + " Year"
-        userId = application.id
-        keys = application.to_dict()['applied']
+        name = application['name']
+        resume = application['resume']
+        email = application['email']
+        phone = application['contactNumber']
+        college = application['college']
+        course = application['course']
+        year = application['yearOfStudy'] + " Year"
+        userId = application['id']
+        keys = application['applied']
         os.system('cls' if os.name == 'nt' else 'clear')
         try: 
             for key in keys:
@@ -261,21 +288,26 @@ if choice == "1":
             input("Press Enter to exit...")
             exit()
 if choice == "2":
-    query = users_ref.where('name', '!=', '')
-    applications = query.stream()
+    applications = []
+    for userId in data:
+        application = data[userId]
+        if "name" in application and "resume" in application:
+            application["id"] = userId
+            applications.append(application)
+            
     os.system('cls' if os.name == 'nt' else 'clear')
     for application in applications:
-        name = application.to_dict()['name']
-        userId = application.id
-        # check if application.to_dict() has key 'resume'
-        if 'resume' in application.to_dict():
-            name = application.to_dict()['name']
-            resume = application.to_dict()['resume']
-            email = application.to_dict()['email']
-            phone = application.to_dict()['contactNumber']
-            college = application.to_dict()['college']
-            course = application.to_dict()['course']
-            year = application.to_dict()['yearOfStudy'] + " Year"
+        name = application['name']
+        userId = application['id']
+        # check if application has key 'resume'
+        if 'resume' in application:
+            name = application['name']
+            resume = application['resume']
+            email = application['email']
+            phone = application['contactNumber']
+            college = application['college']
+            course = application['course']
+            year = application['yearOfStudy'] + " Year"
             print([userId, name, resume, email, phone, college, course, year]);
             with open('applications.csv', 'a', newline='') as file:
                 writer = csv.writer(file)
@@ -326,26 +358,30 @@ if choice == "2":
             ''')
             print("User ID: " + userId)
             print("Name: " + name)
-            input("Press Enter to continue...")
+            input("Press Enter to exit...")
 if choice == "3":
-    query = users_ref.where('resume', '!=', '')
-    applications = query.stream()
+    applications = []
+    for userId in data:
+        application = data[userId]
+        if "name" not in application and "resume" in application:
+            print(application)
+            application["id"] = userId
+            applications.append(application)
     os.system('cls' if os.name == 'nt' else 'clear')
     for application in applications:
-        try: 
-            resume = application.to_dict()['resume']
-            userId = application.id
-            if 'name' not in application.to_dict():
-                print([userId, resume]);
-                with open('applications.csv', 'a', newline='') as file:
-                    writer = csv.writer(file)
-                    # set the header
-                    if os.stat('applications.csv').st_size == 0:
-                        writer.writerow(['userId', 'Resume'])
-                        # leave a blank line
-                        writer.writerow([])
-                    writer.writerow([userId, resume])
-                file.close()
+        try:
+            resume = application['resume']
+            userId = application['id']
+            print([userId, resume]);
+            with open('applications.csv', 'a', newline='') as file:
+                writer = csv.writer(file)
+                # set the header
+                if os.stat('applications.csv').st_size == 0:
+                    writer.writerow(['userId', 'Resume'])
+                    # leave a blank line
+                    writer.writerow([])
+                writer.writerow([userId, resume])
+            file.close()
             time.sleep(1)
         except:
             os.system('cls' if os.name == 'nt' else 'clear')
@@ -387,46 +423,52 @@ if choice == "3":
             input("Press Enter to exit...")
             exit()
 
+if choice == "4":
+    os.system('cls' if os.name == 'nt' else 'clear')
+    # need the show the total number of registered users
+    print("Total number of registered users: " + str(len(data)))
+    input("Press Enter to exit...")
 
-os.system('cls' if os.name == 'nt' else 'clear')
-print('''
-⣻⡿⢏⢥⡻⢬⡍⢳⡘⢦⢣⢝⡢⡓⡜⠰⢂⠐⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢻⣦⠀
-⡿⢏⡜⣲⡙⢦⡙⢧⡙⣎⠞⢦⠡⢁⠈⠀⠀⠄⠀⢈⠀⠈⢀⠈⠀⠈⠐⠈⠀⠁⡀⠁⠈⠀⠄⠁⠀⢁⠂⠄⠀⢀⠀⠀⠉⠲⣄⡀⠹⣧
-⡱⣩⢚⡥⣙⢦⡙⣦⡙⢦⡛⣬⢓⡆⣴⣤⣦⣴⣬⣦⣴⣤⣆⣤⣈⡀⠐⢀⣈⣴⣴⣶⣶⣶⣦⣤⣌⡀⠀⠠⠀⠠⠀⠈⠀⠄⡈⠧⣀⠈
-⣱⢣⢏⡖⣭⢒⡝⢦⣝⢣⠞⣴⣿⠿⣛⣿⣿⣿⣦⡄⠉⠉⠙⢻⣿⣿⣿⣿⠟⠋⠉⣨⣿⣿⣿⣏⠛⠿⣿⣶⣄⠀⠠⠁⠀⠄⡀⠀⠹⡄
-⢎⡳⢎⡜⢦⢫⢜⡣⢎⢧⣛⣿⠋⡀⢾⣿⣿⣿⣿⣿⣶⠀⠀⢀⣼⡿⢿⣾⠗⠀⣾⣿⣿⣿⣿⣿⣧⡀⠈⠛⣿⡧⠀⠀⠁⠐⡀⢁⠂⢜
-⡹⣜⢣⢞⡱⢎⢮⡱⣋⢦⡹⣿⣷⣈⠻⣿⣿⣿⣿⣿⠟⠀⢠⣿⣿⠀⡍⢿⣿⡄⠹⣿⣿⣿⣿⣿⡟⠋⠀⣰⣿⠗⠀⢈⠀⡐⠠⠀⠌⠰
-⠳⣌⢳⢪⡕⣎⠶⡱⣍⠶⡱⢻⣿⣧⣳⡰⠩⠿⠟⡁⢀⣠⣾⡿⡁⢆⠈⢆⡹⣿⣄⣩⠍⡛⠟⠉⠀⣀⣴⣿⠋⠀⡐⠀⠂⠀⠄⠡⠘⡠
-⡹⡌⢧⠳⡜⡜⢮⡱⢎⡳⣍⢣⠞⡽⢻⣿⣷⣷⢶⣶⣾⣿⡿⣵⢏⡶⣍⠶⣰⢹⣿⡙⢻⢷⣶⣶⡿⢿⠛⠁⢀⠠⢀⠠⠁⡈⠄⡁⠂⠁
-⡱⢎⢇⡻⣌⢳⢃⡞⡥⢳⢜⡣⡝⡼⣡⠂⣽⣿⣿⣿⣿⣿⣿⣽⣞⣳⣭⢳⣣⢿⣿⣿⣶⡆⠀⠀⠀⣀⠠⢀⠂⠀⠂⠀⠀⡐⠀⠄⠠⠁
-⡱⢎⢮⡱⢎⡣⢏⡲⣙⢬⡚⡴⣹⢒⠡⣼⣿⣿⣿⣿⣿⣿⣿⣿⡽⣟⣿⣿⣿⣿⣮⣟⣽⣿⣶⣀⠀⠀⠈⠀⠀⠐⠀⠀⠀⠄⡁⢈⠀⠄
-⡱⣋⠶⣱⢋⠶⣩⠖⣭⢢⢓⡱⢎⠢⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⣿⣿⢮⣿⣽⣾⣷⣿⣿⣿⡆⠀⠀⠀⠈⠀⠀⠀⠀⠒⣀⠂⡀⢀
-⡱⢣⡝⢦⣋⠞⣥⠺⣔⢣⡙⢦⢃⡱⢛⢿⡟⣿⡿⣿⠿⣿⣿⣿⣿⣾⣿⣿⡿⣿⢿⣿⣿⣿⡿⠟⠁⠀⠀⠀⠀⠀⠀⢀⠠⠁⠄⢂⠐⠠
-⡱⢣⢎⡳⣌⢏⠶⣩⠖⣣⡙⢆⡇⢆⡉⢂⠉⠐⠡⠉⠊⡙⠿⣿⣿⡟⡿⠏⠓⠉⠋⠉⠋⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠠⠀⠂⠄⣈⠐
-⡱⣃⢮⠱⣎⠺⡜⣥⢛⡤⣙⠶⣘⢢⡑⢢⠈⠄⠐⡀⠄⠀⠁⠐⠀⢈⠀⡁⢀⠠⠄⠀⢀⠀⠀⠀⠀⠐⠀⠀⠀⠀⠀⠀⠄⠂⡈⠐⠠⢈
-⡱⡜⣢⢛⡴⢫⡜⢦⡍⢶⣉⠶⡑⢦⡉⠆⡌⡐⠀⡐⠈⡐⠈⠄⡈⠄⡀⠐⡀⢀⠀⠁⠀⢀⠀⠁⠀⠀⡀⠀⠀⠁⠀⢀⠀⠄⠠⢁⠂⠄
-⡱⢥⢣⡍⣖⢣⠞⡱⢎⠧⡜⢎⡙⢦⡉⠖⡰⢀⠁⠠⠀⠄⡐⢀⠠⠀⡀⠁⡀⠄⠈⠐⡀⠀⠀⢀⠀⠀⠀⠀⠠⠀⠈⠄⠠⢀⠱⠀⡌⠐
-⢧⢋⠖⣱⢪⠱⣎⠵⣫⠜⣜⢣⡝⣢⠙⣆⠱⠀⠌⡀⠂⠄⡀⠄⠀⠂⢀⠁⠀⡐⠈⠀⠄⠐⠈⠀⠀⠈⠀⠀⠀⠄⠡⠈⠄⢂⠄⡃⠤⢁
-⢧⣋⠞⡥⢎⠳⣌⠳⢬⡙⣤⠳⡸⢄⠫⣄⠣⢉⡐⠠⠁⠠⢀⠂⠁⢂⠀⠌⠀⠄⠠⢁⠠⠈⠀⠄⡐⠀⠈⠀⠄⠈⠄⠡⣈⠢⡘⢠⠒⠠
-⣧⢻⡜⣱⢋⠶⣌⡹⣂⠗⡢⢝⡰⢋⡱⢄⠃⢆⠰⠁⠠⠁⢂⠐⠨⡀⢂⠄⠡⠈⡀⢂⠠⢈⠐⠀⠀⢁⠊⡐⡈⠌⡐⠡⣀⠑⡌⢂⠡⢃
-⣭⢳⣭⢣⢏⡞⡴⢣⡕⣫⢱⣊⡔⢣⢰⡈⣔⣨⡐⣌⢠⠐⡀⠌⡀⠡⠀⠂⠄⠁⠀⢀⠠⠀⠀⠁⠈⠀⠀⠁⠌⠢⢅⠣⢄⠣⡘⡌⡑⢢
-⣯⣗⣮⣛⣮⣽⡹⣧⢻⣜⣧⡟⡾⣏⢷⡻⡜⢧⡛⠾⡹⢏⠿⣳⠯⣷⢳⣳⢎⣶⢣⣖⡦⣖⢦⡌⡔⣈⡐⢀⠀⡐⠈⢘⢢⢣⠱⣌⠱⣂
-⣟⣮⢷⣻⣞⣷⣻⣽⣻⣞⣶⣝⢦⣉⢦⡑⣌⢂⠌⡐⠡⢈⠂⡅⡘⢀⠃⠌⠊⠔⡉⠌⠃⠍⠂⡙⠘⠠⢉⠃⠎⡐⢈⡤⣓⢎⡳⣌⠳⣌
-⣿⡽⣯⣷⢿⣞⣯⣷⡿⣯⣿⡾⣿⣽⡾⣽⢾⡽⣮⣵⢻⣬⢳⡜⣜⣣⠻⣜⡛⢶⡱⢎⡖⣦⡱⣤⢲⡰⣆⣜⣲⣜⡳⣞⡭⡞⡵⣎⡟⡼
-⣿⣿⢯⣿⣻⣯⣿⢷⣿⣟⣷⡿⣟⣾⣟⣿⢯⣿⣳⣯⣟⡾⣧⣟⡾⣖⣟⣶⣹⢧⣛⣮⡽⡶⣝⣮⠷⣝⣮⡽⢶⡭⣷⡹⣞⡽⣳⡝⣾⡹
-⣿⣻⡿⣯⣿⣻⣽⣿⣳⡿⣯⣿⣟⣿⢾⣻⡿⣽⣯⣷⣿⣻⣽⣯⣿⣽⣾⣳⣯⣿⣽⣾⣽⣻⣽⣞⡿⣽⡾⣽⢯⣟⣶⣻⡵⣏⡷⣹⠶⣽
+if choice == "1" or choice == "2" or choice == "3":
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print('''
+    ⣻⡿⢏⢥⡻⢬⡍⢳⡘⢦⢣⢝⡢⡓⡜⠰⢂⠐⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢻⣦⠀
+    ⡿⢏⡜⣲⡙⢦⡙⢧⡙⣎⠞⢦⠡⢁⠈⠀⠀⠄⠀⢈⠀⠈⢀⠈⠀⠈⠐⠈⠀⠁⡀⠁⠈⠀⠄⠁⠀⢁⠂⠄⠀⢀⠀⠀⠉⠲⣄⡀⠹⣧
+    ⡱⣩⢚⡥⣙⢦⡙⣦⡙⢦⡛⣬⢓⡆⣴⣤⣦⣴⣬⣦⣴⣤⣆⣤⣈⡀⠐⢀⣈⣴⣴⣶⣶⣶⣦⣤⣌⡀⠀⠠⠀⠠⠀⠈⠀⠄⡈⠧⣀⠈
+    ⣱⢣⢏⡖⣭⢒⡝⢦⣝⢣⠞⣴⣿⠿⣛⣿⣿⣿⣦⡄⠉⠉⠙⢻⣿⣿⣿⣿⠟⠋⠉⣨⣿⣿⣿⣏⠛⠿⣿⣶⣄⠀⠠⠁⠀⠄⡀⠀⠹⡄
+    ⢎⡳⢎⡜⢦⢫⢜⡣⢎⢧⣛⣿⠋⡀⢾⣿⣿⣿⣿⣿⣶⠀⠀⢀⣼⡿⢿⣾⠗⠀⣾⣿⣿⣿⣿⣿⣧⡀⠈⠛⣿⡧⠀⠀⠁⠐⡀⢁⠂⢜
+    ⡹⣜⢣⢞⡱⢎⢮⡱⣋⢦⡹⣿⣷⣈⠻⣿⣿⣿⣿⣿⠟⠀⢠⣿⣿⠀⡍⢿⣿⡄⠹⣿⣿⣿⣿⣿⡟⠋⠀⣰⣿⠗⠀⢈⠀⡐⠠⠀⠌⠰
+    ⠳⣌⢳⢪⡕⣎⠶⡱⣍⠶⡱⢻⣿⣧⣳⡰⠩⠿⠟⡁⢀⣠⣾⡿⡁⢆⠈⢆⡹⣿⣄⣩⠍⡛⠟⠉⠀⣀⣴⣿⠋⠀⡐⠀⠂⠀⠄⠡⠘⡠
+    ⡹⡌⢧⠳⡜⡜⢮⡱⢎⡳⣍⢣⠞⡽⢻⣿⣷⣷⢶⣶⣾⣿⡿⣵⢏⡶⣍⠶⣰⢹⣿⡙⢻⢷⣶⣶⡿⢿⠛⠁⢀⠠⢀⠠⠁⡈⠄⡁⠂⠁
+    ⡱⢎⢇⡻⣌⢳⢃⡞⡥⢳⢜⡣⡝⡼⣡⠂⣽⣿⣿⣿⣿⣿⣿⣽⣞⣳⣭⢳⣣⢿⣿⣿⣶⡆⠀⠀⠀⣀⠠⢀⠂⠀⠂⠀⠀⡐⠀⠄⠠⠁
+    ⡱⢎⢮⡱⢎⡣⢏⡲⣙⢬⡚⡴⣹⢒⠡⣼⣿⣿⣿⣿⣿⣿⣿⣿⡽⣟⣿⣿⣿⣿⣮⣟⣽⣿⣶⣀⠀⠀⠈⠀⠀⠐⠀⠀⠀⠄⡁⢈⠀⠄
+    ⡱⣋⠶⣱⢋⠶⣩⠖⣭⢢⢓⡱⢎⠢⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⣿⣿⢮⣿⣽⣾⣷⣿⣿⣿⡆⠀⠀⠀⠈⠀⠀⠀⠀⠒⣀⠂⡀⢀
+    ⡱⢣⡝⢦⣋⠞⣥⠺⣔⢣⡙⢦⢃⡱⢛⢿⡟⣿⡿⣿⠿⣿⣿⣿⣿⣾⣿⣿⡿⣿⢿⣿⣿⣿⡿⠟⠁⠀⠀⠀⠀⠀⠀⢀⠠⠁⠄⢂⠐⠠
+    ⡱⢣⢎⡳⣌⢏⠶⣩⠖⣣⡙⢆⡇⢆⡉⢂⠉⠐⠡⠉⠊⡙⠿⣿⣿⡟⡿⠏⠓⠉⠋⠉⠋⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠠⠀⠂⠄⣈⠐
+    ⡱⣃⢮⠱⣎⠺⡜⣥⢛⡤⣙⠶⣘⢢⡑⢢⠈⠄⠐⡀⠄⠀⠁⠐⠀⢈⠀⡁⢀⠠⠄⠀⢀⠀⠀⠀⠀⠐⠀⠀⠀⠀⠀⠀⠄⠂⡈⠐⠠⢈
+    ⡱⡜⣢⢛⡴⢫⡜⢦⡍⢶⣉⠶⡑⢦⡉⠆⡌⡐⠀⡐⠈⡐⠈⠄⡈⠄⡀⠐⡀⢀⠀⠁⠀⢀⠀⠁⠀⠀⡀⠀⠀⠁⠀⢀⠀⠄⠠⢁⠂⠄
+    ⡱⢥⢣⡍⣖⢣⠞⡱⢎⠧⡜⢎⡙⢦⡉⠖⡰⢀⠁⠠⠀⠄⡐⢀⠠⠀⡀⠁⡀⠄⠈⠐⡀⠀⠀⢀⠀⠀⠀⠀⠠⠀⠈⠄⠠⢀⠱⠀⡌⠐
+    ⢧⢋⠖⣱⢪⠱⣎⠵⣫⠜⣜⢣⡝⣢⠙⣆⠱⠀⠌⡀⠂⠄⡀⠄⠀⠂⢀⠁⠀⡐⠈⠀⠄⠐⠈⠀⠀⠈⠀⠀⠀⠄⠡⠈⠄⢂⠄⡃⠤⢁
+    ⢧⣋⠞⡥⢎⠳⣌⠳⢬⡙⣤⠳⡸⢄⠫⣄⠣⢉⡐⠠⠁⠠⢀⠂⠁⢂⠀⠌⠀⠄⠠⢁⠠⠈⠀⠄⡐⠀⠈⠀⠄⠈⠄⠡⣈⠢⡘⢠⠒⠠
+    ⣧⢻⡜⣱⢋⠶⣌⡹⣂⠗⡢⢝⡰⢋⡱⢄⠃⢆⠰⠁⠠⠁⢂⠐⠨⡀⢂⠄⠡⠈⡀⢂⠠⢈⠐⠀⠀⢁⠊⡐⡈⠌⡐⠡⣀⠑⡌⢂⠡⢃
+    ⣭⢳⣭⢣⢏⡞⡴⢣⡕⣫⢱⣊⡔⢣⢰⡈⣔⣨⡐⣌⢠⠐⡀⠌⡀⠡⠀⠂⠄⠁⠀⢀⠠⠀⠀⠁⠈⠀⠀⠁⠌⠢⢅⠣⢄⠣⡘⡌⡑⢢
+    ⣯⣗⣮⣛⣮⣽⡹⣧⢻⣜⣧⡟⡾⣏⢷⡻⡜⢧⡛⠾⡹⢏⠿⣳⠯⣷⢳⣳⢎⣶⢣⣖⡦⣖⢦⡌⡔⣈⡐⢀⠀⡐⠈⢘⢢⢣⠱⣌⠱⣂
+    ⣟⣮⢷⣻⣞⣷⣻⣽⣻⣞⣶⣝⢦⣉⢦⡑⣌⢂⠌⡐⠡⢈⠂⡅⡘⢀⠃⠌⠊⠔⡉⠌⠃⠍⠂⡙⠘⠠⢉⠃⠎⡐⢈⡤⣓⢎⡳⣌⠳⣌
+    ⣿⡽⣯⣷⢿⣞⣯⣷⡿⣯⣿⡾⣿⣽⡾⣽⢾⡽⣮⣵⢻⣬⢳⡜⣜⣣⠻⣜⡛⢶⡱⢎⡖⣦⡱⣤⢲⡰⣆⣜⣲⣜⡳⣞⡭⡞⡵⣎⡟⡼
+    ⣿⣿⢯⣿⣻⣯⣿⢷⣿⣟⣷⡿⣟⣾⣟⣿⢯⣿⣳⣯⣟⡾⣧⣟⡾⣖⣟⣶⣹⢧⣛⣮⡽⡶⣝⣮⠷⣝⣮⡽⢶⡭⣷⡹⣞⡽⣳⡝⣾⡹
+    ⣿⣻⡿⣯⣿⣻⣽⣿⣳⡿⣯⣿⣟⣿⢾⣻⡿⣽⣯⣷⣿⣻⣽⣯⣿⣽⣾⣳⣯⣿⣽⣾⣽⣻⣽⣞⡿⣽⡾⣽⢯⣟⣶⣻⡵⣏⡷⣹⠶⣽
 
 
-/$$$$$$$  
-| $$__  $$    
-| $$  \ $$  /$$$$$$  /$$$$$$$   /$$$$$$    
-| $$  | $$ /$$__  $$| $$__  $$ /$$__  $$  
-| $$  | $$| $$  \ $$| $$  \ $$| $$$$$$$$ 
-| $$  | $$| $$  | $$| $$  | $$| $$_____/                                                    
-| $$$$$$$/|  $$$$$$/| $$  | $$|  $$$$$$$                                                    
-|_______/  \______/ |__/  |__/ \_______/     
-                                                                                            
-    File generated as "applications.csv" in the same folder.                                                                                  
-''')
-# wait for user input to exit
-input("Press Enter to exit...")
+    /$$$$$$$  
+    | $$__  $$    
+    | $$  \ $$  /$$$$$$  /$$$$$$$   /$$$$$$    
+    | $$  | $$ /$$__  $$| $$__  $$ /$$__  $$  
+    | $$  | $$| $$  \ $$| $$  \ $$| $$$$$$$$ 
+    | $$  | $$| $$  | $$| $$  | $$| $$_____/                                                    
+    | $$$$$$$/|  $$$$$$/| $$  | $$|  $$$$$$$                                                    
+    |_______/  \______/ |__/  |__/ \_______/     
+                                                                                                
+        File generated as "applications.csv" in the same folder.                                                                                  
+    ''')
+    # wait for user input to exit
+    input("Press Enter to exit...")
